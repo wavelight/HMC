3585d54a21cd2ca12da35ae5e41af244a0c18d79
HeeYo <taboit@naver.com>
2018-03-16 00:44:24 +0900

    1. Cube Updated. IDK what I have done.

--- software/Src/stm32f4xx_hal_msp.c
+++ software/Src/stm32f4xx_hal_msp.c
@@ -289,7 +289,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
                           |GPIO_PIN_12|GPIO_PIN_13;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF1_TIM1;
     HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
