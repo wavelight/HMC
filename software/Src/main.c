90d6eecbbe96eaa823c7da771e9672387e2d4176
HeeYo <taboit@naver.com>
2018-03-17 20:46:34 +0900

    1. testing changed email

--- software/Src/main.c
+++ software/Src/main.c
@@ -143,7 +143,7 @@ int main(void)
 			
 			HAL_GPIO_TogglePin(LED_BLUE_GPIO_Port, LED_BLUE_Pin);
 			HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
-			HAL_GPIO_TogglePin(LED_ORANGE_GPIO_Port, LED_ORANGE_Pin);
+			HAL_GPIO_TogglePin(LED_ORANGE_GPIO_Port, LED_ORANGE_Pin); 
 			HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
 		}		
 	}
